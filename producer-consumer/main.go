36c8b69ad3789dd81a34e73e2a2a00e9fa60507b
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-07-23 15:23:20 +0000

    2024-07-23 22:23:20 +07:00 Asia/Saigon

--- producer-consumer/main.go
+++ producer-consumer/main.go
@@ -4,6 +4,15 @@ const NumberOfPizzas = 10
 
 var pizzasMade, pizzasFailed, total int
 
+type Producer struct {
+	data chan PizzaOrder
+	quit chan chan error
+}
+
+type PizzaOrder struct {
+	pizzaNumber int
+}
+
 func main() {
 
 }
