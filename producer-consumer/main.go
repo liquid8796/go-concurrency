61a58d7e71f57eb815025baf4dcd6d3944183cd2
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2024-07-24 03:05:08 +0000

    2024-07-24 10:05:08 +07:00 Asia/Saigon

--- producer-consumer/main.go
+++ producer-consumer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 
@@ -28,12 +29,60 @@ func (p *Producer) Close() error {
 	return <-ch
 }
 
+func makePizza(pizzaNumber int) *PizzaOrder {
+	pizzaNumber++
+	if pizzaNumber <= NumberOfPizzas {
+		delay := rand.Intn(5) + 1
+		fmt.Printf("Received order #%d!\n", pizzaNumber)
+
+		rnd := rand.Intn(12) + 1
+		msg := ""
+		success := false
+
+		if rnd < 5 {
+			pizzasFailed++
+		} else {
+			pizzasMade++
+		}
+		total++
+
+		fmt.Printf("Making pizza #%d. It will take %d seconds....\n", pizzaNumber, delay)
+		// delay for a bit
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2 {
+			msg = fmt.Sprintf("*** We ran out of ingredients for pizza #%d!", pizzaNumber)
+		} else if rnd <= 4 {
+			msg = fmt.Sprintf("*** The cook quit while making pizza #%d!", pizzaNumber)
+		} else {
+			success = true
+			msg = fmt.Sprintf("Pizza order #%d is ready!", pizzaNumber)
+		}
+
+		p := PizzaOrder{
+			pizzaNumber: pizzaNumber,
+			message:     msg,
+			success:     success,
+		}
+
+		return &p
+	}
+
+	return &PizzaOrder{
+		pizzaNumber: pizzaNumber,
+	}
+}
+
 func pizzeria(pizzaMaker *Producer) {
 	// keep track of which pizza we are making
 	// try to make pizzas
+	var i = 0
+
 	for {
-		// try to make a pizza
-		// decision
+		currentPizza := makePizza(i)
+		if currentPizza != nil {
+
+		}
 	}
 }
 
